43050ac31e481ff34ee9de514d3843ae00544070
Tomi P. Hakala <tomi.hakala@pobox.fi>
2026-06-09 12:43:48 +0300

    Add session listing from agy cache

---
+++ internal/manager/sessions_test.go
@@ -0,0 +1,32 @@
+package manager
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestListSessionsFromCache(t *testing.T) {
+	cache := t.TempDir()
+	data := `{"/home/u/proj":"uuid-1","/home/u/other":"uuid-2"}`
+	if err := os.WriteFile(filepath.Join(cache, "last_conversations.json"), []byte(data), 0o644); err != nil {
+		t.Fatal(err)
+	}
+	sessions, err := readSessions(filepath.Join(cache, "last_conversations.json"), "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sessions) != 2 {
+		t.Fatalf("got %d sessions", len(sessions))
+	}
+}
+
+func TestListSessionsFilteredByDir(t *testing.T) {
+	cache := t.TempDir()
+	data := `{"/home/u/proj":"uuid-1","/home/u/other":"uuid-2"}`
+	_ = os.WriteFile(filepath.Join(cache, "last_conversations.json"), []byte(data), 0o644)
+	sessions, _ := readSessions(filepath.Join(cache, "last_conversations.json"), "/home/u/proj")
+	if len(sessions) != 1 || sessions[0].ConversationID != "uuid-1" {
+		t.Fatalf("got %+v", sessions)
+	}
+}
